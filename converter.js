1d0ff4d19553a3545762aade56ee8645ee891093
Prriya dash <119881879+PrriyaBandita-2002@users.noreply.github.com>
2025-09-29 05:18:58 +0000

    currency converter added

---
+++ converter.js
@@ -0,0 +1,71 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const dropdowns=document.querySelectorAll(".dropdown select");
+const btn=document.querySelector("form button");
+const fromcurr=document.querySelector(".from select");
+const tocurr=document.querySelector(".to select");
+
+const msg=document.querySelector(".msg");
+
+
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+  if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+
+ select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateexchangerate=async ()=>{
+  let amount=document.querySelector(".amount input");
+  let amtval=amount.value;
+  if(amtval==="" || amtval<1)
+  {
+    amtval=1;
+    amount.value="1";
+  }
+  console.log(fromcurr,tocurr);
+
+  const URL = `${BASE_URL}/${fromcurr.value.toLowerCase()}.json`;
+
+let response=await fetch(URL);
+let data=await response.json();
+let rate = data[fromcurr.value.toLowerCase()][tocurr.value.toLowerCase()];
+if (!rate) {
+  msg.innerText = "Exchange rate not available.";
+  return;
+}
+let finalamount=amtval*rate;
+msg.innerText=`${amtval} ${fromcurr.value}=${finalamount} ${tocurr.value}`
+}
+
+const updateFlag=(element)=>{
+    // console.log(element);
+let currCode=element.value;
+let countryCode=countryList[currCode];
+let newsrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
+let img=element.parentElement.querySelector("img");
+img.src=newsrc;
+}
+
+btn.addEventListener("click",(evt)=>{
+  evt.preventDefault();
+  updateexchangerate();
+})
+
+
+window.addEventListener("load",()=>{
+  updateexchangerate();
+});
